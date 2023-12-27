d93bcfb94cc177302166ceafc54e640a0b2fbdb3
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-27 16:21:59 -0600

    308. Creando un Request de Autenticación

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistroRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
     
-    public function register(Request $request){
+    public function register(RegistroRequest $request){
+        //validar registro
+        $data = $request->validate();
+    }
+
+    public function login(RegistroRequest $request){
 
     }
 
-    public function login(Request $request){
-
-    }
-
-    public function logout (Request $request){
+    public function logout (RegistroRequest $request){
         
     }
 
